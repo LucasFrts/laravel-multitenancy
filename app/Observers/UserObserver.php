46203ef5c8e267f45998c1bc51d4bf418be66269
Lucas <lmrfreitas9@gmail.com>
2024-03-20 09:50:32 -0300

    feat: generate tenant database and run migrations from observer

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Observers;
+
+use App\Actions\TenantConnection;
+use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
+    {
+
+        file_put_contents($user->database(), "");
+        info("dabatase criada através do observer");
+        app(TenantConnection::class, ['user' => $user])->execute();
+        Artisan::call('migrate --path=database/migrations/tenant --database=tenant');
+        info(Artisan::output());
+        info("migration concluida através do observer");
+    }
+
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
+}
