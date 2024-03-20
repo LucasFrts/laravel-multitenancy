6a38eb91fa948c144df10ae9a85a01a11d1374b9
Lucas <lmrfreitas9@gmail.com>
2024-03-20 09:47:23 -0300

    multi-tenant connection action

---
+++ app/Actions/TenantConnection.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+readonly class TenantConnection
+{
+    public function __construct(protected User $user)
+    { 
+    }
+
+    public function execute() : void
+    {
+        DB::purge('tenant');
+        config()->set('database.connections.tenant.database', $this->user->database());
+        DB::reconnect('tenant');
+    }
+}
