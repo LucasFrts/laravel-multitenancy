a04476a7539c2fdc52735fe86c18571380eea83e
Lucas <lmrfreitas9@gmail.com>
2024-03-20 09:49:14 -0300

    setup Lead model, migrations and factory

--- app/Models/Tenant/Lead.php
+++ app/Models/Tenant/Lead.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App\Models\Tenanty;
+namespace App\Models\Tenant;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\TenantOwner;
 
 class Lead extends Model
-{
-    // use HasFactory;
+{   
+    use HasFactory, TenantOwner;
 }
